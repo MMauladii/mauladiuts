dd83cf6507e09e80c8379c00a89c961182ad9232
MMauladii <muhammadmauladi@mhs.mdp.ac.id>
2022-11-02 19:47:05 +0700

    blum kelar

---
+++ app/src/main/java/com/example/mauladiuts/SecondMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mauladiuts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_main);
+    }
+}
