96991dea958da710d408e98130cc61874a81f612
MMauladii <muhammadmauladi@mhs.mdp.ac.id>
2022-11-02 23:22:33 +0700

    blumselesai

--- app/src/main/java/com/example/mauladiuts/MainActivity.java
+++ app/src/main/java/com/example/mauladiuts/MainActivity.java
@@ -2,32 +2,41 @@ package com.example.mauladiuts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText etNamaLengkap;
     EditText etNomorPendaftaran;
-    Button btnDaftar;
+    Button btndaftar;
+    CheckBox cbkonfirmasi;
+    Spinner sppendaftaran;
 
-    String NomorPendaftaran, NamaLengkap;
+    String NomorPendaftaran, NamaLengkap, konfirmasi, pendaftaran;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etNomorPendaftaran = findViewById(R.id.et_Nomor_Pendaftaran);
-        etNamaLengkap = findViewById(R.id.et_Nama_Lengkap);
-        btnDaftar = findViewById(R.id.btn_daftar);
+        etNomorPendaftaran = findViewById(R.id.et_NomorPendaftaran);
+        etNamaLengkap = findViewById(R.id.et_NamaLengkap);
+        btndaftar = findViewById(R.id.btn_daftar);
+        cbkonfirmasi = findViewById(R.id.cb_Konfirmasi_dasar);
+        sppendaftaran = findViewById(R.id.sp_Pendaftaran);
 
-        btnDaftar.setOnClickListener(new View.OnClickListener() {
+        btndaftar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NamaLengkap = etNamaLengkap.getText().toString();
                 NomorPendaftaran = etNomorPendaftaran.getText().toString();
+                konfirmasi = cbkonfirmasi.getText().toString();
+                pendaftaran = String.valueOf(sppendaftaran.getSelectedItem());
 
                 if (NamaLengkap.trim().equals("")) {
                     etNamaLengkap.setError("Nama Harus Diisi");
@@ -36,8 +45,19 @@ public class MainActivity extends AppCompatActivity {
                 else if (NomorPendaftaran.trim().equals("")){
                     etNomorPendaftaran.setError("Nomor Pendaftaran Harus Diisi");
                 }
+                else if (!cbkonfirmasi.isChecked()){
+                    Toast.makeText(MainActivity.this, "centang dulu dek!!!!", Toast.LENGTH_SHORT).show();
+                }
+                else if (pendaftaran.equalsIgnoreCase("Jalur Pendaftaran")){
+                    Toast.makeText(MainActivity.this, "Pilih Dulu Dek!!!", Toast.LENGTH_SHORT).show();
+                }
                 else{
-                    Toast.makeText(MainActivity.this, "Selamat " + NamaLengkap + ", Anda Terdaftar!", Toast.LENGTH_SHORT).show();
+                   Intent daftar = new Intent(MainActivity.this,SecondMainActivity.class);
+                   daftar.putExtra("xnama", NamaLengkap);
+                   daftar.putExtra("Xnomor", NomorPendaftaran);
+                   daftar.putExtra("xconfirm",konfirmasi);
+                   daftar.putExtra("xpendaftaran", pendaftaran);
+                   startActivity(daftar);
                 }
             }
         });
--- app/src/main/java/com/example/mauladiuts/SecondMainActivity.java
+++ app/src/main/java/com/example/mauladiuts/SecondMainActivity.java
@@ -2,13 +2,33 @@ package com.example.mauladiuts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondMainActivity extends AppCompatActivity {
+    TextView tvnama;
+    TextView tvNomorPendaftaran;
+    TextView tvpendaftaran;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_main);
+
+        tvnama = findViewById(R.id.tv_namalengkap);
+        Intent terima = getIntent();
+        String terimanama = terima.getStringExtra("xnama");
+        tvnama.setText(terimanama);
+
+        tvNomorPendaftaran = findViewById(R.id.tv_Pendaftaran);
+        Intent baru = getIntent();
+        String terimaNomor = baru.getStringExtra("xnomor");
+        tvNomorPendaftaran.setText(terimaNomor);
+
+        tvpendaftaran = findViewById(R.id.tv_tv);
+        Intent lagi = getIntent();
+        String terimaKode = lagi.getStringExtra("xpendaftaran");
+        tvpendaftaran.setText(terimaKode);
     }
 }
